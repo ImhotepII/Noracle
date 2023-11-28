8cdfb50551bdbdda7b91ad5ec40530492bf8e79b
Charles Isaacs <isaacs.charles@gmail.com>
2023-11-27 22:46:20 -0500

    images and chat bot

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
-
+var path = require('path');
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
 
-
-  if(filename == './') {filename = './index'}
-
-  filename = filename + ".html";
-
-  fs.readFile(filename, function(err, data) {
-    if(err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+    // Default to index.html
+    if (filename == './') {
+        filename = './index.html';
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  })
+
+    var extname = path.extname(filename);
+    var contentType = 'text/html';
+
+    // Set content type based on file extension
+    switch (extname) {
+        case '.jpg':
+        case '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        // Add other content types for different file extensions as needed
+    }
+
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end("404 Not Found");
+            }
+            res.writeHead(500);
+            return res.end('Error loading the file');
+        }
+        res.writeHead(200, {'Content-Type': contentType});
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
 
-console.log("Server Listening on Port 8080....")
+console.log("Server listening on port 8080...");
